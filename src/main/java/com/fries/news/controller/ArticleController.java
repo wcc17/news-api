da611dde7bf17af0bca964e8366e0ba597bb7c39
William Curry <christian.curry17@gmail.com>
2017-05-24 12:08:05 -0400

    Added proper logging

--- src/main/java/com/fries/news/controller/ArticleController.java
+++ src/main/java/com/fries/news/controller/ArticleController.java
@@ -2,6 +2,8 @@ package com.fries.news.controller;
 
 import com.fries.news.domain.Article;
 import com.fries.news.service.ArticleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
@@ -16,29 +18,44 @@ import java.util.List;
 @RestController
 public class ArticleController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(ArticleController.class);
     public static final String BASE_PATH = "/article";
 
     @Autowired
     ArticleService articleService;
 
-
     @RequestMapping(method = RequestMethod.GET, value = BASE_PATH)
     public List<Article> pageOfArticles(@RequestParam("page") Integer pageNumber,
                                         @RequestParam("size") Integer pageSize) {
-        //http://localhost:8080/article?page=0&size=10
-        return articleService.getArticlePage(pageNumber, pageSize);
+        try {
+            //http://localhost:8080/article?page=0&size=10
+            return articleService.getArticlePage(pageNumber, pageSize);
+        } catch (Exception e) {
+            LOGGER.error("Error retrieving a page of articles with page {} and size {}", pageNumber, pageSize, e);
+            return null;
+        }
     }
 
     @RequestMapping(method = RequestMethod.GET, value = BASE_PATH + "/count")
     public Long articleCount() {
-        //http://localhost:8080/article/count
-        return articleService.getArticleCount();
+        try {
+            //http://localhost:8080/article/count
+            return articleService.getArticleCount();
+        } catch (Exception e) {
+            LOGGER.error("Error retrieving article count", e);
+            return null;
+        }
     }
 
     @RequestMapping(method = RequestMethod.GET, value = BASE_PATH + "/top")
     public List<Article> topArticles(@RequestParam("number") Integer numberToRetrieve) {
-        //http://localhost:8080/article/top?number=3
-        return articleService.getTopArticles(numberToRetrieve);
+        try {
+            //http://localhost:8080/article/top?number=3
+            return articleService.getTopArticles(numberToRetrieve);
+        } catch (Exception e) {
+            LOGGER.error("Error retrieving top articles with numberToRetrieve: {}", numberToRetrieve, e);
+            return null;
+        }
     }
 
     //TODO: THIS NEEDS TO BE RESTRICTED IN PRODUCTION
@@ -51,9 +68,8 @@ public class ArticleController {
                     .buildAndExpand(articleId).toUri();
 
             return ResponseEntity.created(location).build();
-        } catch (RuntimeException e) {
-            //TODO: need to log the exception without System.out.println(). Can't remember why Steven said it slows things down
-            System.out.println(e);
+        } catch (Exception e) {
+            LOGGER.error("Error creating article with title {}", article.getTitle(), e);
             return new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
     }
@@ -65,8 +81,7 @@ public class ArticleController {
             articleService.updateArticle(article);
             return ResponseEntity.ok().build();
         } catch (RuntimeException e) {
-            //TODO: need to log the exception without System.out.println(). Can't remember why Steven said it slows things down
-            System.out.println(e);
+            LOGGER.error("Error updating article with id {}", article.getId(), e);
             return new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
     }
@@ -79,21 +94,30 @@ public class ArticleController {
             articleService.deleteArticle(articleId);
             return ResponseEntity.ok().build();
         } catch (RuntimeException e) {
-            //TODO: need to log the exception without System.out.println(). Can't remember why Steven said it slows things down
-            System.out.println(e);
+            LOGGER.error("Error deleting article with id {}", articleId, e);
             return new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
     }
 
     @RequestMapping(method = RequestMethod.GET, value = BASE_PATH, params="id")
     public Article getArticleById(@RequestParam("id") Integer articleId) {
-        //http://localhost:8080/article?id=16
-        return articleService.getArticleById(articleId);
+        try {
+            //http://localhost:8080/article?id=16
+            return articleService.getArticleById(articleId);
+        } catch (Exception e) {
+            LOGGER.error("Error retrieving article with id {}", articleId, e);
+            return null;
+        }
     }
 
     @RequestMapping(method = RequestMethod.GET, value = BASE_PATH, params="name")
     public Article getArticleByName(@RequestParam("name") String articleName) {
-        //http://localhost:8080/article?name=kellyanne-conway-convinced-her-dog-bugged
-        return articleService.getArticleByName(articleName);
+        try {
+            //http://localhost:8080/article?name=testname
+            return articleService.getArticleByName(articleName);
+        } catch (Exception e) {
+            LOGGER.error("Error getting article by name {}", articleName, e);
+            return null;
+        }
     }
 }
--- src/main/java/com/fries/news/util/DateDeserializer.java
+++ src/main/java/com/fries/news/util/DateDeserializer.java
@@ -1,8 +1,11 @@
-package com.fries.news.Util;
+package com.fries.news.util;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fries.news.controller.ArticleController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.time.LocalDate;
@@ -10,14 +13,16 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class DateDeserializer extends JsonDeserializer<LocalDate> {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(DateDeserializer.class);
+
     @Override
     public LocalDate deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
         try {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
             return LocalDate.parse(jp.getText(), formatter);
         } catch (DateTimeParseException e) {
-            //TODO: REVISIT THIS RETURN NULL. also get rid of System.out.print
-            System.out.println(e);
+            LOGGER.error("Error deserializing json date", e);
             return null;
         }
     }
