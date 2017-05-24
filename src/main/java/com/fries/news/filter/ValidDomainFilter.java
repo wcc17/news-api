97b5b0b72ca4a5a9721f19f1e4bb78fb626fd5ac
William Curry <christian.curry17@gmail.com>
2017-05-24 12:27:03 -0400

    fixed ValidDomainFilter

--- src/main/java/com/fries/news/filter/ValidDomainFilter.java
+++ src/main/java/com/fries/news/filter/ValidDomainFilter.java
@@ -1,5 +1,8 @@
 package com.fries.news.filter;
 
+import com.fries.news.controller.ArticleController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,6 +14,8 @@ import java.io.IOException;
 
 @Component
 public class ValidDomainFilter extends OncePerRequestFilter {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(ArticleController.class);
 
     @Override
     protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain)
@@ -27,16 +32,15 @@ public class ValidDomainFilter extends OncePerRequestFilter {
     }
 
     private boolean isValidDomain(String ipAddress, int port){
-        //TODO: need to log this properly without System.out.println
-//        System.out.println(ipAddress);
-//        System.out.println(port);
 
-        //TODO: will need to check for ip of digital ocean server with front-end on it
-//        if(!ipAddress.equals("www.google.com")) {
-//            System.out.println("This is not google.com");
+        //TODO: TO LIMIT WHO CAN MAKE REQUESTS TO THE API
+//        if(!ipAddress.equals("104.236.231.207")) {
+//            LOGGER.error("This is an invalid or unrecognized IP address");
+//            LOGGER.error("IP: " + ipAddress);
+//            LOGGER.error("PORT: " + port);
 //            return false;
 //        }
-//
+
         return true;
     }
 
